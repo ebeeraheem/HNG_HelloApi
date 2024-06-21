a9c29895154c0ffb8d3907880e778063a748a053
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-21 16:17:06 +0100

    Implement HelloController.cs

---
+++ HNG_HelloApi/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG_HelloApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromQuery] string visitor_name)
+    {
+        var name = visitor_name is null ? "client" : visitor_name;
+        var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString();
+        var response = new
+        {
+            client_ip = clientIp ?? "Unknown",
+            greeting = $"Hello, {name}!"
+        };
+
+        return Ok(response);
+    }
+}
